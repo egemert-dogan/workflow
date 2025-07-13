f99634b9795b90c644f3c2ed3c1c92cf3f71155a
saddc_21 <egemertdogan66@gmail.com>
2025-07-13 17:20:57 +0300

    Better workspace and app handling.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 // SIMPLE Workflow Script for Hyprland
 
-// Import libraries
-use hyprland::dispatch::{Dispatch, DispatchType};
+use hyprland::dispatch::{Dispatch, DispatchType, WorkspaceIdentifierWithSpecial};
 use notify_rust::{Hint, Notification};
-use std::{process::exit, thread, time};
+use std::{
+    thread::{self, sleep},
+    time,
+};
 
 struct Window {
     command: String,
@@ -11,77 +13,51 @@ struct Window {
 }
 
 impl Window {
-    fn open(&self) {
-        // Open the app
-        match Dispatch::call(DispatchType::Exec(&self.command.to_string())) {
-            Ok(_) => {
-                println!("Launching app: {}", &self.command);
-            }
-            Err(e) => {
-                eprintln!("Error: {e}");
-            }
-        }
-        // Sleep for 3s
-        println!("Sleeping for 3 seconds");
-        thread::sleep(time::Duration::from_millis(3000));
-        // Send app to the workspace
-        match Dispatch::call(DispatchType::MoveToWorkspaceSilent(
-            hyprland::dispatch::WorkspaceIdentifierWithSpecial::Id(self.workspace),
-            None,
-        )) {
-            Ok(_) => {
-                println!("Moving app to workspace {}", &self.workspace);
-            }
-            Err(e) => {
-                eprintln!("Error: {e}");
-            }
+    async fn start(&self) {
+        match Dispatch::call(DispatchType::Workspace(WorkspaceIdentifierWithSpecial::Id(
+            self.workspace,
+        ))) {
+            Ok(_) => println!("Swicted to workspace"),
+            Err(e) => eprintln!("{e}"),
+        };
+
+        sleep(time::Duration::from_millis(500));
+
+        match Dispatch::call_async(DispatchType::Exec(&self.command.to_string())).await {
+            Ok(_) => println!("ok2"),
+            Err(e) => panic!("{e}"),
         }
     }
 }
 
-fn main() {
-    // Define the editor
+#[tokio::main]
+async fn main() {
     let editor = Window {
-        command: String::from("alacritty --hold -e nvim ~/projects/"),
+        command: String::from("alacritty -e nvim"),
         workspace: 1,
     };
-    // Define the browser
     let browser = Window {
         command: String::from("firefox"),
         workspace: 2,
     };
-    // Define the music player
     let music = Window {
         command: String::from("youtube-music"),
         workspace: 3,
     };
-    // Define the note taking app
     let note = Window {
         command: String::from("md.obsidian.Obsidian"),
-        workspace: 4,
+        workspace: 3,
     };
-    // Open the editor and sleep for 1s
-    editor.open();
-    thread::sleep(time::Duration::from_millis(1000));
-    // Open the browser and sleep for 1s
-    browser.open();
-    thread::sleep(time::Duration::from_millis(1000));
-    // Open the music player and sleep 1s
-    music.open();
-    thread::sleep(time::Duration::from_millis(1000));
-    // Open the note taking app
-    note.open();
 
-    println!("Done.");
+    editor.start().await;
+    browser.start().await;
+    music.start().await;
+    note.start().await;
 
-    // Send notification
-    thread::sleep(time::Duration::from_millis(1000));
+    thread::sleep(time::Duration::from_millis(500));
     let _ = Notification::new()
         .summary("Workflow")
         .body("is ready to use.")
         .hint(Hint::Resident(true))
         .show();
-
-    // Exit
-    exit(0);
 }
